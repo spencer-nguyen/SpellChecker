817d4afcf6f84dcf3c21f19c34330a16b55e7525
Spencer Nguyen <shn1@students.uwf.edu>
2021-05-05 20:50:37 -0500

    saving work

---
+++ SpellChecker/src/HashTable.java
@@ -0,0 +1,54 @@
+/*********************************************************************** 
+  Student Name: Spencer Nguyen
+  File Name: SpellChecker
+  Assignment Number: Project 4
+  Course: COP5416
+
+This class creates a hash table...
+
+************************************************************************/
+public class HashTable {
+	
+	private int[] table;
+	
+	HashTable(int tableSize){
+		
+		table = new int[tableSize];
+		
+	}
+	
+	
+	public void insertKey(String key){
+		
+		
+	}
+	
+	public boolean isKeyInTable(String word) {
+
+		boolean keyIsInTable;
+		
+		
+		
+		return keyIsInTable;
+	}
+	
+	private int hashFunction(int key){
+		
+		int bucket;
+		
+		
+		return bucket;
+	}
+	
+
+	
+	private int keyStringToNum(String key) {
+		
+		for (int i = 0; i < key.length(); i++) {
+			
+		}
+		
+	}
+	
+	
+}
